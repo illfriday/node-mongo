94a6db29f318effadfc80dc0cbd4b65b7d3c3589
illfriday <willprouty@gmail.com>
2022-04-08 13:05:45 -0400

    refactor nested callback to use ES6 promises

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // requires MongoDB Node Driver and it's MongoClient MODULE. so ap can act as a CLIENT for MONGO SERVER.
 const MongoClient = require("mongodb").MongoClient;
-//assert NODE CORE MODULE
-const assert = require("assert").strict;
+//assert() NODE CORE MODULE No longer used to test for errors with ES6 PROMISE-based approach
+//assert() No longer used to test for errors with ES6 PROMISE-based approach
+
 //import our NODE MODULE 'operations'
 const dboper = require("./operations");
 
@@ -10,67 +11,119 @@ const url = "mongodb://localhost:27017/";
 //DATABASE NAME
 const dbname = "nucampsite";
 
-//Connect MONGO CLIENT to MONGODB server. URL, configuration, CALLBACK FUNCTION with error handling
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  //assert() can perform various checks here we are checking the EXPECTED VALUE of err(ERROR) against 'null'. So if the value of 'err' === null, we ASSERT we have connected successfully. if the ASSERTION fails, this causes the ap to terminate and throw an ERROR
-  assert.strictEqual(err, null);
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected correctly to the server.");
 
-  console.log("Connected correctly to server.");
-  //this connects to the CLiENT to MongoDB 'nucampsite'
-  const db = client.db(dbname);
+    const db = client.db(dbname);
 
-  //WORKS*******
-  // if (db.collection("campsites")) {
-  //   db.dropCollection("campsites", (err, result) => {
-  //     assert.strictEqual(err, null);
-  //     console.log("Dropped Collection:", result);
+    db.dropCollection("campsites")
+      .then((result) => {
+        console.log("Dropped collection:", result);
+      })
+      .catch((err) => console.log("No collection to drop."));
 
-  //     /****** */
-  //     //Recreate fresh instance of the 'campsites' COLLECTION
-  //     //MOVED TO OPERATIONS
-  //     // const collection = db.collection("campsites");
-
-  //   });
-  // }
-  //DELETE all docs in the 'campsite collection' bc we have been messing with it/start fresh w/o docs from a previous test
-  db.dropCollection("campsites", (err, result) => {
-    assert.strictEqual(err, null);
-    console.log("Dropped Collection:", result);
-    //     //insert a DOCUMENT into the COLLECTION. We give a JSON STRING and a CALLBACK FUNCTION to test for errors
-    dboper.insertDocument(
-      db,
-      { name: "Breadcrumb Trail Campground", description: "On fire." },
-      "campsites",
-      (result) => {
+    dboper
+      .insertDocument(
+        db,
+        { name: "Breadcrumb Trail Campground", description: "On fire!" },
+        "campsites"
+      )
+      .then((result) => {
         console.log("Insert Document:", result.ops);
 
-        dboper.findDocuments(db, "campsites", (docs) => {
-          console.log("Found Documents:", docs);
+        return dboper.findDocument(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Documents:", docs);
 
-          dboper.updateDocument(
-            db,
-            { name: "Breadcrumb Trail Campground" },
-            { description: "Not on fire anymore." },
-            "campsites",
-            (result) => {
-              console.log("Updated document count:", result.result.nModified);
+        return dboper.updateDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          { description: "No longer on fire!!" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Updated Document Count:", result.result.nModified);
 
-              dboper.findDocuments(db, "campsites", (docs) => {
-                console.log("Found Documents:", docs);
+        return dboper.findDocument(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Documents:", docs);
 
-                dboper.removeDocument(
-                  db,
-                  { name: "Breadcrumb Trail Campground" },
-                  "campsites",
-                  (result) => {
-                    console.log("Deleted Document Count:", result.deletedCount);
-                  }
-                );
-              });
-            }
-          );
-        });
-      }
-    );
-  });
-});
+        return dboper.removeDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Deleted Document Count:", result.deletedCount);
+
+        return client.close();
+      })
+      .catch((err) => {
+        console.log(err);
+        client.close();
+      });
+  })
+  .catch((err) => console.log(err));
+
+//*****Code below uses NESTED CALLBACKS to work around NODE'S NON_BLOCKING nature. Each NESTED CALLBACK is explicitly told to wait until the previous CALLBACK has returned a value/completed it's task. This results in complex code, which can be simplified using ES6 PROMISES, chained .then().catch() METHODS which act as placeholders for VALUES THAT ARE NOT YET AVAILABLE and handling errors if they never become available. This is called a PROMISE CHAIN. ES6 PROMISES are natively supported/built into NODE. CODE RE-WRITTEN ABOVE^^^^^ */
+
+//Connect MONGO CLIENT to MONGODB server. URL, configuration, CALLBACK FUNCTION with error handling
+
+// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+
+//   //assert() can perform various checks here we are checking the EXPECTED VALUE of err(ERROR) against 'null'. So if the value of 'err' === null, we ASSERT we have connected successfully. if the ASSERTION fails, this causes the ap to terminate and throw an ERROR
+//   assert.strictEqual(err, null);
+
+//   console.log("Connected correctly to server.");
+//   //this connects to the CLiENT to MongoDB 'nucampsite'
+
+//   const db = client.db(dbname);
+
+//DELETE all docs in the 'campsite collection' bc we have been messing with it/start fresh w/o docs from a previous test
+//   db.dropCollection("campsites", (err, result) => {
+//     assert.strictEqual(err, null);
+//     console.log("Dropped Collection:", result);
+
+//     //     //insert a DOCUMENT into the COLLECTION. We give a JSON STRING and a CALLBACK FUNCTION to test for errors
+//     dboper.insertDocument(
+//       db,
+//       { name: "Breadcrumb Trail Campground", description: "On fire." },
+//       "campsites",
+//       (result) => {
+//         console.log("Insert Document:", result.ops);
+
+//         dboper.findDocuments(db, "campsites", (docs) => {
+//           console.log("Found Documents:", docs);
+
+//           dboper.updateDocument(
+//             db,
+//             { name: "Breadcrumb Trail Campground" },
+//             { description: "Not on fire anymore." },
+//             "campsites",
+//             (result) => {
+//               console.log("Updated document count:", result.result.nModified);
+
+//               dboper.findDocuments(db, "campsites", (docs) => {
+//                 console.log("Found Documents:", docs);
+
+//                 dboper.removeDocument(
+//                   db,
+//                   { name: "Breadcrumb Trail Campground" },
+//                   "campsites",
+//                   (result) => {
+//                     console.log("Deleted Document Count:", result.deletedCount);
+//                   }
+//                 );
+//               });
+//             }
+//           );
+//         });
+//       }
+//     );
+//   });
+// });
--- operations.js
+++ operations.js
@@ -1,44 +1,69 @@
 //this file is a NODE MODULE containing our DATABASE METHODS
 //CREATE-insert...READ-find... UPDATE... DELETE-remove
-const assert = require("assert").strict;
+
+//assert() No longer used to test for errors with ES6 PROMISE-based approach
+// const assert = require("assert").strict;
+
+//BELOW Code uses ES6 PROMISES. By removing the callback arguments of our operations, NODE AUTOMATICALLY RETURNS A PROMISE(BUILT-IN FUNCTIONALITY OF NODE DRIVER API). Result in greatly simplified code.
+
+exports.insertDocument = (db, document, collection) => {
+  const coll = db.collection(collection);
+  return coll.insertOne(document);
+};
+
+exports.findDocument = (db, collection) => {
+  const coll = db.collection(collection);
+  return coll.find().toArray();
+};
+exports.removeDocument = (db, document, collection) => {
+  const coll = db.collection(collection);
+  return coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+  const coll = db.collection(collection);
+  return coll.updateOne(document, { $set: update }, null);
+};
+
+//****BELOW Code uses CALLBACKS. Updated ^^^above^^^^ to use ES6 PROMISES */
 
 //set up our insertDocument() METHOD and EXPORT it
-exports.insertDocument = (db, document, collection, callback) => {
-  const coll = db.collection(collection);
+// exports.insertDocument = (db, document, collection, callback) => {
+//   const coll = db.collection(collection);
 
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
-};
+//   coll.insertOne(document, (err, result) => {
+//     assert.strictEqual(err, null);
+//     callback(result);
+//   });
+// };
 
-//set up our findDocuments() METHOD and EXPORT it
-exports.findDocuments = (db, collection, callback) => {
-  const coll = db.collection(collection);
+// //set up our findDocuments() METHOD and EXPORT it
+// exports.findDocuments = (db, collection, callback) => {
+//   const coll = db.collection(collection);
 
-  coll.find().toArray((err, docs) => {
-    assert.strictEqual(err, null);
-    callback(docs);
-  });
-};
+//   coll.find().toArray((err, docs) => {
+//     assert.strictEqual(err, null);
+//     callback(docs);
+//   });
+// };
 
-//set up our removeDocument() METHOD and EXPORT it
-exports.removeDocument = (db, document, collection, callback) => {
-  const coll = db.collection(collection);
+// //set up our removeDocument() METHOD and EXPORT it
+// exports.removeDocument = (db, document, collection, callback) => {
+//   const coll = db.collection(collection);
 
-  coll.deleteOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
-};
+//   coll.deleteOne(document, (err, result) => {
+//     assert.strictEqual(err, null);
+//     callback(result);
+//   });
+// };
 
 //set up our updateDocument() METHOD and EXPORT it
-//updateDocument() has 4 parameters: document, information about UPDATE(UPDATE OPERATOR $set from MONGODB overwrite information)m configs9not used here, CALLBACK FUNCTION
-exports.updateDocument = (db, document, update, collection, callback) => {
-  const coll = db.collection(collection);
+//updateDocument() has 4 parameters: document, information about UPDATE(UPDATE OPERATOR $set from MONGODB overwrite information), configs(not used here), CALLBACK FUNCTION
+// exports.updateDocument = (db, document, update, collection, callback) => {
+//   const coll = db.collection(collection);
 
-  coll.updateOne(document, { $set: update }, null, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
-};
+//   coll.updateOne(document, { $set: update }, null, (err, result) => {
+//     assert.strictEqual(err, null);
+//     callback(result);
+//   });
+// };
